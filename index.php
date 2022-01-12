a09a3e086f1d2d4520a5fa674458ca671ae4d8b4
Muhammad Talha Zaroon <mrtzaroon@gmail.com>
2022-01-12 18:13:50 +0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Website 2/title>
+    <title>Website 2</title>
 </head>
 <body>
         <h1><?php echo 'Website 2 - Description'; ?></h1>
